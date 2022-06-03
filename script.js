b2cedfa1fe99b0aedfab6635af5275686cfcc66c
tiagoDev7 <thiagoerick1@hotmail.com>
2022-06-03 11:48:08 +0200

    js updated

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+document.getElementsByClassName("boxy-animal")[0].style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr5Tyu1ckQBNoBCnXwddJoYgraucyVYqVS6w&usqp=CAU)";
+document.getElementsByClassName("boxy-sport")[0].style.backgroundImage = "url(https://myigcseworksheets.files.wordpress.com/2019/02/surf_featured_1.jpg)";
+document.getElementsByClassName("boxy-fictional")[0].style.backgroundImage = "url(https://i.jeuxactus.com/datas/jeux/d/r/dragon-ball-xenoverse-2/xl/dragon-ball-xenoverse-2-6250b9adeb2c5.jpg)";
+document.getElementsByClassName("boxy-celeb")[0].style.backgroundImage = "url(https://cdn.museum.nl/assets/f2675914-1463-4b8e-9103-2bf9709ea062?w=1280&c=3347b33857c49db85f0f2344d44b3c1dba9e2ba68e674b3c7edfec542c48c8af)";
+////////// change words!!
 
+let btn = document.getElementsByClassName("button")[3];
+
+btn.onclick = function(){
+    change()
+};
+
+function change() {
+
+    let wordOne = document.getElementsByClassName("column")[0];
+    let wordTwo = document.getElementsByClassName("column")[1];
+    let wordThree = document.getElementsByClassName("column")[2];
+
+    let arrayWords = "";
+};
 
 // gets all "column" classes
 let defineColumn = document.getElementsByClassName("column");
